805fbe7f2141e44f5a0483ed6a4ee2dd7b00a6da
Roger Melo <rogerw592@gmail.com>
2017-08-16 00:36:13 -0300

    Aula 05, exercício 01 adicionado

--- js/menu.js
+++ js/menu.js
@@ -2,3 +2,25 @@ function pega(elemento) {
 
     return document.querySelector(elemento);
 }
+
+
+var html = pega("html");
+var botaoMenuAbrir = pega(".menu-abrir");
+var botaoMenuFechar = pega(".menu-fechar");
+
+botaoMenuAbrir.addEventListener("click", function() {
+
+    html.classList.add("menu-ativo");
+});
+
+botaoMenuFechar.addEventListener("click", function() {
+
+    html.classList.remove("menu-ativo");
+});
+
+html.addEventListener("click", function(event) {
+
+    if (event.target === this) {
+        html.classList.remove("menu-ativo");
+    }
+});
