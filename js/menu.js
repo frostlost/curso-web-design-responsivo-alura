0b9b29724d91cad1ab6d380eb35ac0e181e421e3
Roger Melo <rogerw592@gmail.com>
2017-08-25 17:31:52 -0300

    Menu Buttons display and position fixed

--- js/menu.js
+++ js/menu.js
@@ -11,16 +11,19 @@ var botaoMenuFechar = pega(".menu-fechar");
 botaoMenuAbrir.addEventListener("click", function() {
 
     html.classList.add("menu-ativo");
+    botaoMenuFechar.classList.remove('invisivel');
 });
 
 botaoMenuFechar.addEventListener("click", function() {
 
     html.classList.remove("menu-ativo");
+    this.classList.add('invisivel');
 });
 
 html.addEventListener("click", function(event) {
 
     if (event.target === this) {
         html.classList.remove("menu-ativo");
+        botaoMenuFechar.classList.add('invisivel');
     }
 });
